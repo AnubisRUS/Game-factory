50054300ad11fdaa43b295a976426c47d413cd28
Mark <80956787+MGorbl@users.noreply.github.com>
2021-06-16 14:58:26 +0300

    Add files via upload

---
+++ sprites/mainmenu.py
@@ -0,0 +1,19 @@
+import pygame, sys
+
+class Button(pygame.sprite.Sprite):
+
+    def __init__(self, size, image="", pos=(0, 0)):
+        super().__init__()
+
+        #create button
+        self.image = pygame.Surface(size)
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+
+        surface = pygame.display.get_surface()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
