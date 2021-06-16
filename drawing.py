3312101d5545f2d2a35893b750226073dd8a3091
Mark <80956787+MGorbl@users.noreply.github.com>
2021-06-16 16:14:56 +0300

    Delete drawing.py

--- drawing.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from settings import *
-class Drawing:#класс для рисования объектов
-    def __init__(self, sc):
-        self.sc = sc
-        self.Trigermenu = True#тригер работы меню
-    def menu(self):#рисование меню
-        pass
