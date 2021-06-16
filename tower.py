f8f311619193cc63acdc6b49135f2de0a442bac2
AnubisRUS <85950720+AnubisRUS@users.noreply.github.com>
2021-06-16 08:53:07 +0300

    Add files via upload

---
+++ tower.py
@@ -0,0 +1,9 @@
+import pygame
+from settings import *
+class Tower:
+    def __init__(self):
+        self.health = tower_health # Жизни башни
+    def enemy_collid(self):#проверка насоприкосновение с врагом
+        pass
+    def draw_tower(self):#рисуем башню
+        pass
