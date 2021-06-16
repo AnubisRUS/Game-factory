9ec166e55f64f141efe01b85c94ca823d3c88194
kingarthurrr <85954347+kingarthurrr@users.noreply.github.com>
2021-06-16 10:58:36 -0400

    Update mage.py

--- mage.py
+++ mage.py
@@ -1,23 +1,26 @@
+#imports
 import pygame
+
+#Mage
 class Mage(pygame.sprite.Sprite):
     def __init__(self, spells):
         super().__init__()
-        self.image = pygame.image.load(r"assets\images\loremipsum.png")
+        self.image = pygame.image.load(r"assets\images\mage.png")
         self.rect = self.image.get_rect()
         self.spells = 5
-        self.health = 200
         self.last_shot = pygame.time.get_ticks()
         self.cooldown = 1000
+        self.repulsion = 0
+        self.slowdown = 1
         fallen_enemies = 0
         screen = pygame.display.get_surface()
         self.rect.x = 0
         self.rect.y = 0
+
+#Functions
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
     def update(self):
         key = pygame.key.get_pressed()
         now = pygame.time.get_ticks()
-    def shoot(self):
-        pass
-    def level_up(self):
-        pass
