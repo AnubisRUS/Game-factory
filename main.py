2e2c70bf20c18d7d8cfe8aa0904c8840f9ffd5c1
AnubisRUS <85950720+AnubisRUS@users.noreply.github.com>
2021-06-16 09:07:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import pygame
+from settings import *
+from drawing import *
+from tower import *
 pygame.init()
+sc = pygame.display.set_mode((width_window, height_window))#полотно размером с весь экран
+drawing = Drawing(sc)#назначения класса для рисования
+drawing.menu()#рисование меню
