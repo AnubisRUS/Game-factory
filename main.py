019f34877f7e8f9cb3aa5c904c669df1ce326cb9
AnubisRUS <85950720+AnubisRUS@users.noreply.github.com>
2021-06-16 16:11:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,104 @@
 import pygame
 from settings import *
-from drawing import *
 from tower import *
+import pygame, sys
+from settings import *
+
+#import sprites
+from buttons import *
+
 pygame.init()
-sc = pygame.display.set_mode((width_window, height_window))#полотно размером с весь экран
-drawing = Drawing(sc)#назначения класса для рисования
-drawing.menu()#рисование меню
+
+#creating window
+screen = pygame.display.set_mode((width_window, height_window))
+clock = pygame.time.Clock()
+
+#show menu func
+def mainmenu():
+    #fonts
+    arial = pygame.font.SysFont("Arial", 72)
+
+    #sprites
+    game_caption = arial.render("Fall of Darkness", True, (255, 255, 255))
+    playbutton = Button((396, 98), "", (402, 281))
+    aboutbutton = Button((396, 98), "", (402, 443))
+    exitbutton = Button((238, 81), "", (922, 580))
+
+    running = True
+    while running:
+
+        clock.tick(FPS)
+
+        #events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if playbutton.rect.collidepoint(pygame.mouse.get_pos()):
+                    startgame()
+                if aboutbutton.rect.collidepoint(pygame.mouse.get_pos()):
+                    pass
+                if exitbutton.rect.collidepoint(pygame.mouse.get_pos()):
+                    pygame.quit()
+                    sys.exit()
+
+        #rendering
+        screen.blit(game_caption, (410, 95))
+        playbutton.draw(screen)
+        aboutbutton.draw(screen)
+        exitbutton.draw(screen)
+
+        #updates
+        pygame.display.update()
+
+
+
+#start game func
+def startgame():
+    # fonts
+    arial = pygame.font.SysFont("Arial", 72)
+
+    #sprites
+    test_text = arial.render("GameFactory", True, (255, 255, 255))
+    running = True
+    screen.blit(test_text, (410, 300))
+    clock.sleep(5)
+    while running:
+        clock.tick(FPS)
+        # events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        # updates
+        screen.fill((0, 0, 0))
+        pygame.display.update()
+
+def aboutmenu():
+    # fonts
+    arial = pygame.font.SysFont("Arial", 72)
+
+    #sprites
+
+    running = True
+    while running:
+
+        clock.tick(FPS)
+
+        #events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # rendering
+
+        # update
+        pygame.display.update()
+
+
+
+#showing menu
+mainmenu()
