02da2f2098507e3f7a61f7cd0634535f942ea2b6
kingarthurrr <85954347+kingarthurrr@users.noreply.github.com>
2021-06-17 10:01:43 -0400

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,10 +1,16 @@
 import pygame
 from settings import *
 
-class Enemy:
+class Monster(pygame.sprite.Sprite):
     def __init__(self, hp, speed, cost):
+        super().__init__()
         self.hp = hp
         self.speed = speed
         self.cost = cost
         self.image = pygame.image.load(r"assets\images\enemy.png")
         self.rect = self.image.get_rect()
+        screen = pygame.display.get_surface()
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+    def update(self):
+        self.rect.x -= self.speed
