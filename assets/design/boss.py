ff27a36683b21c10fc45fe3141557e7a31f0927c
AnubisRUS <85950720+AnubisRUS@users.noreply.github.com>
2021-06-20 16:25:01 +0300

    Delete boss.py

--- assets/design/boss.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from enemy import Monster
-from settings import *
-class Boss(Monster):
-    def __init__(self, x):
-        super().__init__()
-        self.hp = boss_hp
-        self.cost = boss_cost
